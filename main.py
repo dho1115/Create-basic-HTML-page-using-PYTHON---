03ad75b0dca98f19601151022e70aaecc21fd072
admin_dho <davidhorph@hotmail.com>
2025-01-22 14:49:11 -0600

    Added .gitigore.

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 My github repo can be viewed here: https://github.com/dho1115/Create-basic-HTML-page-using-PYTHON---
 """
 
-
 try:
    #Information that will be displayed onto index.html.
    companyName= input ("Input the Company Name!!! ")
